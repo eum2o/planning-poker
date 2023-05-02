1df4cbdc142f3e8d98ec56630bb7a475e207e600
eum2o <msc-gh.eum2o@aleeas.com>
2023-05-02 19:10:31 +0200

    dont modify users state when sorting by estimation

--- src/components/ParticipantList.jsx
+++ src/components/ParticipantList.jsx
@@ -6,41 +6,40 @@ import { SocketContext } from "../context/socket";
 function ParticipantList({ users }) {
   const socket = useContext(SocketContext);
 
-  // check if all users have submitted their estimations
   const allEstimationsSubmitted = users.every(
     (user) => user.estimation !== NO_ESTIMATION
   );
 
-  // sort users by estimation if allEstimationsSubmitted is true
-  const sortedUsers = allEstimationsSubmitted
-    ? users.sort((a, b) => a.estimation - b.estimation)
-    : users;
+  const getSortedUsers = (users) => {
+    if (allEstimationsSubmitted) {
+      return [...users].sort((a, b) => a.estimation - b.estimation);
+    }
+    return users;
+  };
+
+  const sortedUsers = getSortedUsers(users);
 
   return (
     <table className="ParticipantList">
       <tbody>
-        {sortedUsers.map(
-          (user) => (
-            (
-              <tr
-                key={user.name}
-                className={user.socketId === socket.id ? "current-user" : ""}
-              >
-                <td>
-                  {user.estimation === NO_ESTIMATION ? "🤔" : "✅"}
-                  <span className="user-name">{user.name}</span>
-                </td>
-                {user.socketId === socket.id || allEstimationsSubmitted ? (
-                  <td className="user-estimation">
-                    <b>{valueToCardLabel[user.estimation]}</b>
-                  </td>
-                ) : (
-                  <td className="user-estimation-placeholder">&nbsp;&nbsp;</td>
-                )}
-              </tr>
-            )
-          )
-        )}
+        {sortedUsers.map((user) => (
+          <tr
+            key={user.name}
+            className={user.socketId === socket.id ? "current-user" : ""}
+          >
+            <td>
+              {user.estimation === NO_ESTIMATION ? "🤔" : "✅"}
+              <span className="user-name">{user.name}</span>
+            </td>
+            {user.socketId === socket.id || allEstimationsSubmitted ? (
+              <td className="user-estimation">
+                <b>{valueToCardLabel[user.estimation]}</b>
+              </td>
+            ) : (
+              <td className="user-estimation-placeholder">&nbsp;&nbsp;</td>
+            )}
+          </tr>
+        ))}
       </tbody>
     </table>
   );
