9285956e8f7a675fdc1af0469c5dabebdd6dd0a9
RANJITH ROSAN <91519971+RANJITHROSAN17@users.noreply.github.com>
2023-05-07 13:43:48 +0530

    Update server.test.js

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,3 +1,20 @@
-test("addUser should add a new user", () => {
-  // TODO really no idea how to run the real server and test it
+ // TODO really no idea how to run the real server and test it
+  const request = require('supertest');
+const app = require('../app');
+
+describe('POST /users', () => {
+  it('should add a new user', async () => {
+    const res = await request(app)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: 'password'
+      })
+      .expect(201);
+      
+    expect(res.body).toHaveProperty('id');
+    expect(res.body.name).toEqual('John Doe');
+    expect(res.body.email).toEqual('johndoe@example.com');
+  });
 });
